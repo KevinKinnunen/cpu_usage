cb54f00824be005858b2b9c52e19df73277e0607
KevinKinnunen <54663592+KevinKinnunen@users.noreply.github.com>
2021-12-19 00:13:41 +0100

    Update 12/19-21
    Fixed
    + cpu_usage.service
    + cpu_usage.py
    
    Updated:
    + README.md

--- README.md
+++ README.md
@@ -1,38 +1,42 @@
-###How program works
+#How program works
 E.g. 
 ./cpu_usage.py -M 20 -T 10
 -M (memory check, if memory is higher than e.g. 20, it will beep)
 -T (timer, 10 = 10 sec, its required).
 
-cpu_usage.rapport.txt is used to loggs information:
+cpu_usage.rapport.txt is used to logg information:
 - If app is running (beep once)
 - PID, Name, Memory
 - Was entered memory higher or lower than memory (higher = beep sound)
 
-###Access to root
+#Access to root
 sudo su -
 
 sudo nano /etc/systemd/system/cpu_usage_service
 sudo nano cpu_usage.py
 
-###Files and path
-/root/cpu_usage.py
+#Files and path
 /home/kevin/cpu_usage_rapport.txt
+/home/kevin/cpu_usage.py
+/etc/systemd/system/cpu_usage_service
 
-###Reload daemon
+#Reload daemon
 sudo systemctl daemon-reload
 
-###Enable service
+#Enable service
 sudo systemctl enable cpu_usage.service
 
-###Start service
+#Start service
 sudo systemctl start cpu_usage.service
 
-###Stop service
+#Stop service
 sudo systemctl stop cpu_usage.service
 
-###Restart service
+#Restart service
 sudo systemctl restart cpu_usage.service
 
-###Check status of service
+#Check status of service
 sudo systemctl status cpu_usage.service
+
+#Disable service
+sudo systemctl disable cpu_usage.service
--- cpu_usage.py
+++ cpu_usage.py
@@ -6,7 +6,7 @@ import argparse
 import time
 
 beep = lambda x: os.system("echo -n '\a';sleep 0.2;" * x)
-beep(1)
+beep(1) 
 
 a = "cpu_usage.py is running!\n"
 i = str(["PID", "name", "memory"])
@@ -23,7 +23,7 @@ print("GBs = {}".format(float(x)))
 
 parser = argparse.ArgumentParser(description='Memory check within sec/min')
 parser.add_argument('-M', '--memory', type=float, metavar='', help='Memory check (MB/GBs)')
-parser.add_argument('-T', '--timer', type=int, metavar='', required=True, help='Timer to send info (60 = 1min)')
+parser.add_argument('-T', '--timer', type=int, metavar='', help='Timer to send info (60 = 1min)')
 args = parser.parse_args()
 
 def print_memoryinfo():
@@ -38,6 +38,8 @@ def print_memoryinfo():
             pass
 
 def mem(memory, timer):
+    memory = 50 #Mbs
+    timer = 3600 #5min
     if (memory > 0):
         x = (process.memory_info().rss/1024.0**2)
     elif (memory <= 0):
@@ -54,9 +56,27 @@ def mem(memory, timer):
             file.write(i + "\n")
             return mem
 
+def mem2(memory, timer):
+    memory = 125 #1Gb in Mbs
+    timer = 3600 #60min
+    if (memory > 0):
+        x = (process.memory_info().rss/1024.0**2)
+    elif (memory <= 0):
+        x = (process.memory_info().rss/1024.0**3)
+    if (x >= memory):
+        beep(1)
+    if (timer):
+        time.sleep(timer)
+        with open("cpu_usage_rapport.txt", "a+") as file:
+            if (x >= memory):
+                file.write("Memory is higher than {}\n".format(float(memory)))
+            elif (x <= memory):
+                file.write("Memory is lower than {}\n".format(float(memory)))
+            file.write(i + "\n")
+            return mem2
+
 if __name__ == '__main__':
-while True:
-    print("./cpu_usage.py -M 10 -T 60")
-    print(mem(args.memory, args.timer))
-    print_memoryinfo()
-time.sleep(10)
+    while True:
+        print(mem(args.memory, args.timer))
+        print(mem2(args.memory, args.timer))
+        print_memoryinfo()
